ee66bb0854098423b03ec4cf2f4426a713e1c5e3
Marcus Harrison <marcus@harrisonland.co.uk>
2024-11-03 11:01:14 +0100

    Initial commit

---
+++ src/lex.c
@@ -0,0 +1,188 @@
+#include "scallop-lang/lex.h"
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+#define void_fn scallop_lang_void_fn
+#define lex_fn scallop_lang_lex_fn
+
+typedef enum {
+	CLASS_EOF,
+	CLASS_WORD,
+	CLASS_WORD_SEPARATOR,
+	CLASS_STATEMENT_SEPARATOR,
+	CLASS_ESCAPE,
+	CLASS_SINGLE_QUOTE,
+	CLASS_DOUBLE_QUOTE,
+	CLASS_BEGIN_CURLY_BLOCK,
+	CLASS_END_CURLY_BLOCK,
+	CLASS_BEGIN_SQUARE_BLOCK,
+	CLASS_END_SQUARE_BLOCK,
+
+	CLASS_UNKNOWN,
+} CHARACTER_CLASS;
+
+static CHARACTER_CLASS get_class(wint_t c)
+{
+	const bool is_word
+		= isalnum(c)
+		|| c == '-'
+		|| c == '_';
+
+	if (is_word) {
+		return CLASS_WORD;
+	}
+
+	switch (c) {
+		case ' ':
+		case '\t':
+			return CLASS_WORD_SEPARATOR;
+		case '\r':
+		case '\n':
+		case ';':
+			return CLASS_STATEMENT_SEPARATOR;
+		case '\\':
+			return CLASS_ESCAPE;
+		case '\'':
+			return CLASS_SINGLE_QUOTE;
+		case '"':
+			return CLASS_DOUBLE_QUOTE;
+		case '{':
+			return CLASS_BEGIN_CURLY_BLOCK;
+		case '}':
+			return CLASS_END_CURLY_BLOCK;
+		case '[':
+			return CLASS_BEGIN_SQUARE_BLOCK;
+		case ']':
+			return CLASS_END_SQUARE_BLOCK;
+	}
+
+	if (c == WEOF)
+		return CLASS_EOF;
+
+	return CLASS_UNKNOWN;
+}
+
+static lex_fn *default_context(wint_t input)
+{
+	switch (get_class(input)) {
+		case CLASS_EOF:
+			return scallop_lang_lex_end;
+		case CLASS_WORD:
+			return scallop_lang_lex_word;
+		case CLASS_WORD_SEPARATOR:
+			return scallop_lang_lex_word_separator;
+		case CLASS_ESCAPE:
+			return scallop_lang_lex_escape;
+		case CLASS_SINGLE_QUOTE:
+			return scallop_lang_lex_single_quote;
+		default:
+			return scallop_lang_lex_unexpected;
+	}
+}
+
+static lex_fn *single_quoted_context(wint_t input)
+{
+	if (input == WEOF)
+		return scallop_lang_lex_unexpected;
+	if (input == '\'')
+		return scallop_lang_lex_end_single_quote;
+	return scallop_lang_lex_single_quoted_word;
+}
+
+static lex_fn *double_quoted_context(wint_t input)
+{
+	if (input == WEOF)
+		return scallop_lang_lex_unexpected;
+	if (input == '"')
+		return scallop_lang_lex_end_double_quote;
+	return scallop_lang_lex_double_quoted_word;
+}
+
+static void_fn *lex_end_impl(wint_t c)
+{
+	(void)c;
+	abort();
+	return (void_fn *)&lex_end_impl;
+}
+
+static void_fn *lex_unexpected_impl(wint_t c)
+{
+	return lex_end_impl(c);
+}
+
+lex_fn *const scallop_lang_lex_end = (lex_fn *)&lex_end_impl;
+lex_fn *const scallop_lang_lex_unexpected = (lex_fn *)&lex_unexpected_impl;
+
+lex_fn *scallop_lang_lex_begin(wint_t input)
+{
+	return default_context(input);
+}
+
+void_fn *scallop_lang_lex_word(wint_t input)
+{
+	return (void_fn *)default_context(input);
+}
+
+void_fn *scallop_lang_lex_word_separator(wint_t input)
+{
+	return (void_fn *)default_context(input);
+}
+
+void_fn *scallop_lang_lex_escape(wint_t input)
+{
+	if (input == WEOF)
+		return (void_fn *)scallop_lang_lex_unexpected;
+	return (void_fn *)scallop_lang_lex_word;
+}
+
+void_fn *scallop_lang_lex_single_quote(wint_t input)
+{
+	return (void_fn *)single_quoted_context(input);
+}
+
+void_fn *scallop_lang_lex_single_quoted_word(wint_t input)
+{
+	return (void_fn *)single_quoted_context(input);
+}
+
+void_fn *scallop_lang_lex_end_single_quote(wint_t input)
+{
+	return (void_fn *)default_context(input);
+}
+
+void_fn *scallop_lang_lex_double_quote(wint_t input)
+{
+	return (void_fn *)double_quoted_context(input);
+}
+
+void_fn *scallop_lang_lex_double_quoted_word(wint_t input)
+{
+	return (void_fn *)double_quoted_context(input);
+}
+
+void_fn *scallop_lang_lex_end_double_quote(wint_t input)
+{
+	return (void_fn *)default_context(input);
+}
+
+void_fn *scallop_lang_lex_begin_curly_block(wint_t input)
+{
+	return (void_fn *)default_context(input);
+}
+
+void_fn *scallop_lang_lex_end_curly_block(wint_t input)
+{
+	return (void_fn *)default_context(input);
+}
+
+void_fn *scallop_lang_lex_begin_square_block(wint_t input)
+{
+	return (void_fn *)default_context(input);
+}
+
+void_fn *scallop_lang_lex_end_square_block(wint_t input)
+{
+	return (void_fn *)default_context(input);
+}
---
+++ src/scallop-lang/lex.h
@@ -0,0 +1,219 @@
+/*
+ * Scallop - A Shell Language for Parallelization (Language Definition)
+ * Copyright (C) 2024  Marcus Harrison
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+\
+#ifndef SCALLOP_LANG_LEX
+#define SCALLOP_LANG_LEX
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <wchar.h>
+
+/**
+ * \file
+ */
+
+/**
+ * \brief Type definition for a lexer state function.
+ *
+ * The lexer state machine functions take a wide character input and
+ * return a lexer state function. The return value should
+ * be cast to a scallop_lang_lex_fn* before use.
+ *
+ * The entry of the finite state machine is
+ * scallop_lang_lex_begin().
+ *
+ * If the current state receives an unexpected input, it will
+ * return scallop_lang_lex_unexpected().
+ *
+ * You _must_ check that the return value of the current
+ * state is scallop_lang_lex_unexpected() and handle it
+ * correctly. Calling scallop_lang_lex_unexpected() will
+ * call abort().
+ *
+ * scallop_lang_lex_end() indicates that the state machine
+ * has terminated and no further states apply. You
+ * _must_ check for scallop_lang_lex_end() to indicate
+ * that processing finished successfully.
+ *
+ * Example:
+ * \code
+ * #include <stdlib.h>
+ * #include <stdio.h>
+ *
+ * #define lex_fn scallop_lang_lex_fn
+ * #define begin scallop_lang_lex_begin
+ * #define end scallop_lang_lex_end
+ * #define unexpected scallop_lang_lex_unexpected
+ *
+ * int main()
+ * {
+ * 	lex_fn *old_current = NULL;
+ * 	int current_character = 0;
+ * 	int current_state_character = 0;
+ * 	for (
+ * 		lex_fn *current = begin(getwchar());
+ * 		current != end;
+ * 		current = (lex_fn)current(getwchar())
+ * 	) {
+ * 		if (current == unexpected) {
+ * 			// Handle error
+ * 			return 1;
+ * 		}
+ *
+ * 		// do processing
+ * 		// In this example, we're just reporting
+ * 		// new state transitions, nothing more complex
+ * 		if (current != old_current) {
+ * 			printf(
+ * 				"State change at: %d\n"
+ * 				"Previous state length: %d\n",
+ * 				current_character,
+ * 				current_state_character
+ * 			);
+ * 			current_state_character = 0;
+ * 		}
+ * 		current_character++;
+ * 		current_state_character++;
+ * 	}
+ *
+ * 	return 0;
+ * }
+ * \endcode
+ */
+typedef void *scallop_lang_void_fn();
+typedef void *(*scallop_lang_lex_fn(wint_t))();
+
+scallop_lang_lex_fn *scallop_lang_lex_begin(wint_t input);
+
+extern scallop_lang_lex_fn *const scallop_lang_lex_end;
+extern scallop_lang_lex_fn *const scallop_lang_lex_unexpected;
+
+/**
+ * \brief Represents an unquoted word.
+ *
+ * \param input The next wide character input.
+ * \returns A pointer to the next state function.
+ */
+scallop_lang_void_fn *scallop_lang_lex_word(wint_t input);
+
+/**
+ * \brief Represents a word separator.
+ *
+ * \param input The next wide character input.
+ * \returns A pointer to the next state function.
+ */
+scallop_lang_void_fn *scallop_lang_lex_word_separator(wint_t input);
+
+/**
+ * \brief Represents an escape character, '\'.
+ *
+ * \param input The next wide character input.
+ * \returns A pointer to the next state function.
+ */
+scallop_lang_void_fn *scallop_lang_lex_escape(wint_t input);
+
+/**
+ * \brief Represents an opening single quote.
+ *
+ * \param input The next wide character input.
+ * \returns A pointer to the next state function.
+ */
+scallop_lang_void_fn *scallop_lang_lex_single_quote(wint_t input);
+
+/**
+ * \brief Represents a single quote, closing a single-quote string.
+ *
+ * \param input The next wide character input.
+ * \returns A pointer to the next state function.
+ */
+scallop_lang_void_fn *scallop_lang_lex_end_single_quote(wint_t input);
+
+/**
+ * \brief Represents characters in a single-quoted string
+ * 	which contribute to a word.
+ *
+ * \param input The next wide character input.
+ * \returns A pointer to the next state function.
+ */
+scallop_lang_void_fn *scallop_lang_lex_single_quoted_word(wint_t input);
+
+/**
+ * \brief Represents an opening double quote.
+ *
+ * \param input The next wide character input.
+ * \returns A pointer to the next state function.
+ */
+scallop_lang_void_fn *scallop_lang_lex_double_quote(wint_t input);
+
+/**
+ * \brief Represents a double quote, closing a double-quote string.
+ *
+ * \param input The next wide character input.
+ * \returns A pointer to the next state function.
+ */
+scallop_lang_void_fn *scallop_lang_lex_end_double_quote(wint_t input);
+
+/**
+ * \brief Represents characters in a double-quoted string
+ * 	which contribute to a word.
+ *
+ * \param input The next wide character input.
+ * \returns A pointer to the next state function.
+ */
+scallop_lang_void_fn *scallop_lang_lex_double_quoted_word(wint_t input);
+
+/**
+ * \brief Represents the opening of a curly bracket block '{'.
+ *
+ * \param input The next wide character input.
+ * \returns A pointer to the next state function.
+ */
+scallop_lang_void_fn *scallop_lang_lex_open_curly_block(wint_t input);
+
+/**
+ * \brief Represents the closing of a curly bracket block '}'.
+ *
+ * \param input The next wide character input.
+ * \returns A pointer to the next state function.
+ */
+scallop_lang_void_fn *scallop_lang_lex_close_curly_block(wint_t input);
+
+/**
+ * \brief Represents the opening of a squre bracket block '['.
+ *
+ * \param input The next wide character input.
+ * \returns A pointer to the next state function.
+ */
+scallop_lang_void_fn *scallop_lang_lex_open_squre_block(wint_t input);
+
+/**
+ * \brief Represents the closing of a squre bracket block ']'.
+ *
+ * \param input The next wide character input.
+ * \returns A pointer to the next state function.
+ */
+scallop_lang_void_fn *scallop_lang_lex_close_squre_block(wint_t input);
+
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif // SCALLOP_LANG_LEX
