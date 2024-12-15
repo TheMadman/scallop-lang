c4ea9d21b3d0b97532ab1d8eb3876374f1825b33
TheMadman <marcus@harrisonland.co.uk>
2024-12-15 20:45:10 +0000

    Deploying to gh-pages from @ TheMadman/scallop-lang@50c72757ba98ca94cec7d14a8ad80e67252839e4 🚀

--- search/functions_0.js
+++ search/functions_0.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['scallop_5flang_5ftoken_5finit_0',['scallop_lang_token_init',['../token_8h.html#a9d2e119e13e10d3f463d964cb038fbbd',1,'token.h']]],
   ['scallop_5flang_5ftoken_5fnext_1',['scallop_lang_token_next',['../token_8h.html#a77e6a972eb1af7c3c7ebea16809fc62f',1,'token.h']]],
-  ['scallop_5flang_5ftoken_5fnext_5fraw_2',['scallop_lang_token_next_raw',['../token_8h.html#a1580273010a6f1b767be3379094fec29',1,'token.h']]]
+  ['scallop_5flang_5ftoken_5fnext_5fraw_2',['scallop_lang_token_next_raw',['../token_8h.html#a1580273010a6f1b767be3379094fec29',1,'token.h']]],
+  ['scallop_5flang_5ftoken_5fnormalize_5fword_3',['scallop_lang_token_normalize_word',['../token_8h.html#a2d7e4e6cd2bfc2047339083787c31980',1,'token.h']]]
 ];
